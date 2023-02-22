3cb16446451a5f3362122e15666c3409b88fa2ef
gloatingorc <gloatingorc@gmail.com>
2023-02-22 16:26:12 +0100

    stash

--- index.php
+++ index.php
@@ -3,15 +3,15 @@ session_start();
 require './database.php';
 
 if (isset($_POST) && !empty($_POST)) {
-  if (isset($_POST['register']) && !empty($_POST['register'])) {
-    # code...
-  } else if (isset($_POST['login']) && !empty($_POST['login'])) {
-    # code...
-  } else {
-    # code...
-  }
+        if (isset($_POST['register']) && !empty($_POST['register'])) {
+                # code...
+        } else if (isset($_POST['login']) && !empty($_POST['login'])) {
+                # code...
+        } else {
+                # code...
+        }
 } else {
-  header('Location: ' . $_SERVER['HTTP_REFERER'] . $_SERVER['REQUEST_URI'] . 'accueil.php');
+        header('Location: ' . $_SERVER['HTTP_REFERER'] . $_SERVER['REQUEST_URI'] . 'accueil.php');
 }
 
 
