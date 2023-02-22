9f8ce25bda09bef60c3f2f5d7305a8fb8c2a7aa7
Meutevive <christianmbappe033@gmail.com>
2023-02-22 16:03:07 +0100

    ✨ doing login and register page

--- login.php
+++ login.php
@@ -1,72 +1,84 @@
 <?php
-session_start()
 
+session_start()
 ?>
 
 
-<!-- Section: Design Block -->
-<section class=" text-center text-lg-start">
-    <style>
-        .rounded-t-5 {
-            border-top-left-radius: 0.5rem;
-            border-top-right-radius: 0.5rem;
-        }
 
-        @media (min-width: 992px) {
-            .rounded-tr-lg-0 {
-                border-top-right-radius: 0;
+
+<!DOCTYPE html>
+<html lang="en">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- Section: Design Block -->
+    <section class="text-center text-lg-start">
+        <style>
+            .cascading-right {
+                margin-right: -50px;
             }
 
-            .rounded-bl-lg-5 {
-                border-bottom-left-radius: 0.5rem;
+            @media (max-width: 991.98px) {
+                .cascading-right {
+                    margin-right: 0;
+                }
             }
-        }
-    </style>
-    <div class="card mb-3">
-        <div class="row g-0 d-flex align-items-center">
-            <div class="col-lg-4 d-none d-lg-flex">
-                <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" alt="Trendy Pants and Shoes" class="w-100 rounded-t-5 rounded-tr-lg-0 rounded-bl-lg-5" />
-            </div>
-            <div class="col-lg-8">
-                <div class="card-body py-5 px-md-5">
+        </style>
 
-                    <form>
-                        <!-- Email input -->
-                        <div class="form-outline mb-4">
-                            <input type="email" id="form2Example1" class="form-control" />
-                            <label class="form-label" for="form2Example1">Email address</label>
-                        </div>
+        <!-- Jumbotron -->
+        <div class="container py-4">
+            <div class="row g-0 align-items-center">
+                <div class="col-lg-6 mb-5 mb-lg-0">
+                    <div class="card cascading-right" style="
+            background: hsla(0, 0%, 100%, 0.55);
+            backdrop-filter: blur(30px);
+            ">
+                        <div class="card-body p-5 shadow-5 text-center">
+                            <h2 class="fw-bold mb-5">Connexion</h2>
+                            <form method="POST" action="index.php">
+                                <!-- 2 column grid layout with text inputs for the first and last names -->
 
-                        <!-- Password input -->
-                        <div class="form-outline mb-4">
-                            <input type="password" id="form2Example2" class="form-control" />
-                            <label class="form-label" for="form2Example2">Password</label>
-                        </div>
-
-                        <!-- 2 column grid layout for inline styling -->
-                        <div class="row mb-4">
-                            <div class="col d-flex justify-content-center">
-                                <!-- Checkbox -->
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-                                    <label class="form-check-label" for="form2Example31"> Remember me </label>
+                                <!-- Email input -->
+                                <div class="form-outline mb-4">
+                                    <input type="email" id="form3Example3" class="form-control" name="username" />
+                                    <label class="form-label" for="form3Example3">Identifiant</label>
                                 </div>
-                            </div>
 
-                            <div class="col">
-                                <!-- Simple link -->
-                                <a href="#!">Forgot password?</a>
-                            </div>
+                                <!-- Password input -->
+                                <div class="form-outline mb-4">
+                                    <input type="password" id="form3Example4" class="form-control" name="password" />
+                                    <label class="form-label" for="form3Example4">Mot de passe</label>
+                                </div>
+
+                                <!-- Submit button -->
+                                <input type="submit" class="btn btn-primary btn-block mb-4" value=" Connexion">
+                                   
+                                
+                                <!-- Register buttons -->
+                                <div class="text-center">
+                                    <a href="register.php">Crée un compte</a>
+                                </div>
+                            </form>
                         </div>
+                    </div>
+                </div>
 
-                        <!-- Submit button -->
-                        <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
-
-                    </form>
-
+                <div class="col-lg-6 mb-5 mb-lg-0">
+                    <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" class="w-100 rounded-4 shadow-4" alt="" />
                 </div>
             </div>
         </div>
-    </div>
-</section>
-<!-- Section: Design Block -->
+        <!-- Jumbotron -->
+    </section>
+    <!-- Section: Design Block -->
+
+</body>
+
+</html>
